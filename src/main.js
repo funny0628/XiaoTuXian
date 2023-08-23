83bdad3e1389ab0c621b7b38fad52e565f6439cf
qinfang <610531115@qq.com>
2023-08-23 22:47:09 +0800

    axios封装

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+//测试接口函数
+import { getCategory } from './apis/testAPI'
+getCategory().then(res=>{
+  console.log(res,'res---');
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
