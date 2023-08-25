a10d8bad372bd063a6f262c6c19a1025ee821f19
qinfang <610531115@qq.com>
2023-08-25 22:57:37 +0800

    Layout静态页面搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,10 +1,15 @@
 <script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue';
 </script>
 
 <template>
-  <div>我是首页</div>
+  <LayoutNav/>
+  <LayoutHeader/>
   <router-view></router-view>
+  <LayoutFooter/>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 </style>
