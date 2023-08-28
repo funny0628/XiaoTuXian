8c5665f6981762489f53eb921f306b9dc3de4dbc
qinfang <610531115@qq.com>
2023-08-28 22:00:26 +0800

    home-图片懒加载自定义指令实现

--- src/main.js
+++ src/main.js
@@ -6,9 +6,30 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router/index'
 
+//vueUse检测元素是否出现在视口中
+import { useIntersectionObserver } from '@vueuse/core'
+
 import App from './App.vue'
 
 const app = createApp(App)
+app.directive('img-lazy', {
+  /**
+   * 
+   * @param {*} el:指令绑定的那个元素 img
+   * @param {*} binding:binding.value 指令等于后面绑定的表达式的值 图片的url
+   */
+  mounted(el,binding){
+    useIntersectionObserver(
+      el,
+      ([{ isIntersecting }]) => {
+        if(isIntersecting){
+          //进入视口区域
+          el.src = binding.value
+        }
+      },
+    )
+  }
+})
 
 app.use(createPinia())
 app.use(router)
--- src/views/Home/components/HomeHot.vue
+++ src/views/Home/components/HomeHot.vue
@@ -17,7 +17,7 @@ onMounted(() => getHotList())
     <ul class="goods-list">
       <li v-for="item in hotList" :key="item.id">
         <RouterLink to="/">
-          <img :src="item.picture" v-img-lazy="item.picture" alt="">
+          <img :src="item.pictures" v-img-lazy="item.picture" alt="">
           <p class="name">{{ item.title }}</p>
           <p class="desc">{{ item.alt }}</p>
         </RouterLink>
