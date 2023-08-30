25c6e8b9aab029d30ddb6f05510e6f91d45172e4
qinfang <610531115@qq.com>
2023-08-30 20:54:45 +0800

    放大镜-查看商品详情图片

--- src/components/imageView/index.vue
+++ src/components/imageView/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useMouseInElement } from '@vueuse/core'
  
 // 图片列表
@@ -20,6 +20,8 @@ const enterhandler = (i) => {
  
 // 2. 获取鼠标相对位置
 const target = ref(null)
+const layer = ref(null)
+const large = ref(null)
 const { elementX, elementY, isOutside } = useMouseInElement(target)
  
 // 3. 控制滑块跟随鼠标移动（监听elementX/Y变化，一旦变化 重新设置left/top）
@@ -28,27 +30,42 @@ const top = ref(0)
  
 const positionX = ref(0)
 const positionY = ref(0)
+
+
+const subSmalWidth = ref(0)
+const subBigWidth = ref(0)
+const subSmalHeight = ref(0)
+const subBigHeight = ref(0)
+const countWidth = ref(0)
+const countHeight = ref(0)
+
 watch([elementX, elementY, isOutside], () => {
+  subSmalWidth.value = layer.value.offsetWidth / 2;
+  subBigWidth.value = target.value.offsetWidth - subSmalWidth.value;
+  subSmalHeight.value = layer.value.offsetHeight / 2;
+  subBigHeight.value = target.value.offsetHeight - subSmalHeight.value;
+  countWidth.value = target.value.offsetWidth - layer.value.offsetWidth;
+  countHeight.value = target.value.offsetHeight - layer.value.offsetHeight;
  
   // 如果鼠标没有移入到盒子里面 直接不执行后面的逻辑
   if (isOutside.value) return
  
   // 有效范围内控制滑块距离
   // 横向
-  if (elementX.value > 100 && elementX.value < 300) {
-    left.value = elementX.value - 100
+  if (elementX.value > subSmalWidth.value && elementX.value < subBigWidth.value) {
+    left.value = elementX.value - subSmalWidth.value
   }
   // 纵向
-  if (elementY.value > 100 && elementY.value < 300) {
-    top.value = elementY.value - 100
+  if (elementY.value > subSmalHeight.value && elementY.value < subBigHeight.value) {
+    top.value = elementY.value - subSmalHeight.value
   }
  
   // 处理边界
-  if (elementX.value > 300) { left.value = 200 }
-  if (elementX.value < 100) { left.value = 0 }
+  if (elementX.value > subBigWidth.value) { left.value = countWidth.value }
+  if (elementX.value <  subSmalWidth.value) { left.value = 0 }
  
-  if (elementY.value > 300) { top.value = 200 }
-  if (elementY.value < 100) { top.value = 0 }
+  if (elementY.value > subBigHeight.value) { top.value = countHeight.value }
+  if (elementY.value < subSmalHeight.value) { top.value = 0 }
  
   // 控制大图的显示
   positionX.value = -left.value * 2
@@ -66,7 +83,7 @@ watch([elementX, elementY, isOutside], () => {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+      <div  ref="layer" class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -75,9 +92,9 @@ watch([elementX, elementY, isOutside], () => {
       </li>
     </ul>
     <!-- 放大镜大图 -->
-    <div class="large" :style="[
+    <div ref="large" class="large" :style="[
       {
-        backgroundImage: `url(${imageList[0]})`,
+        backgroundImage: `url(${imageList[activeIndex]})`,
         backgroundPositionX: `${positionX}px`,
         backgroundPositionY: `${positionY}px`,
       },
